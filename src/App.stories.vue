e93a4906158991778f17fccde1112df34ba6db53
Jeremiah Koeiman <9001603@student.zadkine.nl>
2021-01-30 16:26:05 +0100

    Storybook finally working

---
+++ src/App.stories.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="apps">
+    @{{ user.username }} - {{ fullName }}
+    <strong>Followers: </strong> {{ followers }}
+    <button @click="followUser">
+      Follow
+    </button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data () {
+      return {
+        followers: 0,
+        user: {
+          id: 1,
+          username: 'Jerry',
+          firstname: 'Jerry',
+          lastname: 'Boy',
+          email: 'jerryboy@gmail.com',
+          isAdmin: true
+        }
+      }
+    },
+    computed: {
+      fullName: function () {
+        return `${this.user.firstname} ${this.user.lastname}`
+      }
+    },
+    methods: {
+      followUser() {
+        this.followers++
+      }
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+}
+</style>
