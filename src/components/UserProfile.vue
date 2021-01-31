054655176b99a50c5e91990936959f5e6cf55cf1
Jeremiah Koeiman <9001603@student.zadkine.nl>
2021-01-31 14:29:36 +0100

    Chapter 5: Forms & Handling User Input

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -8,6 +8,23 @@
       <div class="user-profile__follower-count">
         <strong>Followers: </strong> {{ followers }}
       </div>
+      <form class="user-profile__create-twoot" @submit.prevent="createNewTwoot">
+        <label for="newTwoot"><strong>New Twoot</strong></label>
+        <textarea id="newTwoot" rows="4" v-model="newTwootContent"/>
+
+        <div class="user-profile__create-twoot-type">
+          <label for="newTwootType"><strong>Type: </strong></label>
+          <select id="newTwootType" v-model="selectedTwootType">
+            <option :value="option.value" v-for="(option, index) in twootTypes" :key="index">
+              {{ option.name }}
+            </option>
+          </select>
+        </div>
+
+        <button>
+          Twoot!
+        </button>
+      </form>
     </div>
     <div class="user-profile__twoots-wrapper">
       <TwootItem v-for="twoot in user.twoots"
@@ -30,6 +47,18 @@ export default {
   },
   data () {
     return {
+      newTwootContent: '',
+      selectedTwootType: 'instant',
+      twootTypes: [
+        {
+          value: 'draft',
+          name: 'Draft'
+        },
+        {
+          value: 'instant',
+          name: 'Instant Twoot'
+        }
+      ],
       followers: 0,
       user: {
         id: 1,
@@ -69,6 +98,15 @@ export default {
     },
     toggleFavorite(id) {
       console.log(`Favourited Twoot #${id}`)
+    },
+    createNewTwoot() {
+      if (this.newTwootContent && this.selectedTwootType !== 'draft') {
+        this.user.twoots.unshift({
+          id: this.user.twoots.length + 1,
+          content: this.newTwootContent
+        })
+        this.newTwootContent = '';
+      }
     }
   },
   mounted() {
@@ -110,5 +148,11 @@ h1 {
 .user-profile__twoots-wrapper {
   display: grid;
   grid-gap: 10px;
+}
+
+.user-profile__create-twoot {
+  padding-top: 20px;
+  display: flex;
+  flex-direction: column;
 }
 </style>
